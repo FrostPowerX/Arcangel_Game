942830e74b8a0e02dd9a499e3da5fe4b41331754
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-15 16:35:55 -0300

    cacho

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,29 +1,16 @@
 extends CharacterBody2D
 
 @export var upVelMulti: float = 1
-<<<<<<< HEAD
 @export var speed: float = 600
-=======
-@export var speed: float = 0
-@export var maxLife: float= 3
-@export var life: float= maxLife
->>>>>>> Tomi_Test
 
 @onready var health_system: Node = $HealthSystem
 
 var useVelMulti: float = 0
 
-<<<<<<< HEAD
 func _process(_delta):
 	if(!health_system.IsAlive()):
 		Die()
 	
-=======
-func _ready() -> void:
-	add_to_group("PlayerGroup")
-
-
->>>>>>> Tomi_Test
 func Movement(_delta):
 	var directionX = Input.get_axis("left","right")
 	
@@ -33,10 +20,7 @@ func Movement(_delta):
 		useVelMulti = upVelMulti * 0.6
 	else:
 		useVelMulti = upVelMulti
-		
 	velocity = Vector2(directionX * speed, -useVelMulti * speed)
-	
-	
 	#print(velocity)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
